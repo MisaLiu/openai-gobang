c0ecaad77f74010051456ca6aecf0f1774914b02
Misa Liu <misaliu@misaliu.top>
2025-12-02 04:40:01 +0800

    fix(backend): Fix dump piece position converting problem

--- packages/backend/src/utils/chess.ts
+++ packages/backend/src/utils/chess.ts
@@ -1,7 +1,7 @@
 import { ChessBoardLabelsRow } from '../const';
 import type { ChessPiece } from '../types';
 
-const StringParseReg = /^(black|white):([A-Z])([\d]{1,2})$/;
+const StringParseReg = /(black|white):([A-Z])([\d]{1,2})/;
 
 export const parseString = (string: string): ChessPiece | null => {
   const match = string.match(StringParseReg);
@@ -15,7 +15,7 @@ export const parseString = (string: string): ChessPiece | null => {
   return {
     type: type as 'black' | 'white',
     row: rowId,
-    column: parseInt(column),
+    column: parseInt(column) - 1,
   }
 };
 
@@ -23,5 +23,5 @@ export const buildString = (piece: ChessPiece): string => {
   const rowLabel = ChessBoardLabelsRow[piece.row];
   if (!rowLabel) throw new Error('Invalid row index');
 
-  return `${piece.type}:${rowLabel}${piece.column}`;
+  return `${piece.type}:${rowLabel}${piece.column + 1}`;
 };
