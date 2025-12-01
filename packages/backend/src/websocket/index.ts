de2d2a5e6694fa025fc0e7f6fe1e60cc2d0fa5c2
Misa Liu <misaliu@misaliu.top>
2025-12-02 00:12:12 +0800

    refactor(backend/websocket): Better path detection implement

--- packages/backend/src/websocket/index.ts
+++ packages/backend/src/websocket/index.ts
@@ -1,8 +1,8 @@
 import { WebSocketServer } from 'ws';
 import type WebSocket from 'ws';
 
-export const createWebSocketServer = (pathname: string, options?: WebSocket.ServerOptions) => {
-  const wss = new WebSocketServer(options);
+export const createWebSocketServer = (pathname: string, { server, ...options }: WebSocket.ServerOptions) => {
+  const wss = new WebSocketServer({ ...options, noServer: true });
 
   wss.on('connection', (ws, req) => {
     if ((req.url || '/') !== pathname) {
@@ -11,5 +11,16 @@ export const createWebSocketServer = (pathname: string, options?: WebSocket.Serv
 
     ws.send('Hello world!');
     console.log('A WebSocket client has connected');
+  });
+  
+  if (!server) return;
+  server.on('upgrade', (req, socket, head) => {
+    if ((req.url || '/') !== pathname) {
+      return socket.destroy();
+    }
+
+    wss.handleUpgrade(req, socket, head, (ws) => {
+      wss.emit('connection', ws, req);
+    });
   });
 };
